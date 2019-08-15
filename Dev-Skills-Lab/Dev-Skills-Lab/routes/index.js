b415aff3e67f94dffaeead61a704f24acbc222c5
snsaleh1 <snsaleh11@gmail.com>
2019-08-15 12:21:46 -0500

    test commit

--- Dev-Skills-Lab/Dev-Skills-Lab/routes/index.js
+++ Dev-Skills-Lab/Dev-Skills-Lab/routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 var skillsCtrl = require('../controllers/skills');
 
-router.get('/', skillsCtrl.index);
+router.get('/', skillsCtrl.index); // this is the homepage 
 router.get('/new', skillsCtrl.new);
-router.get('/:id', skillsCtrl.show);
+router.get('/:id', skillsCtrl.show); // imagine its skills/1 
 router.get('/:id/edit', skillsCtrl.edit);
 router.post('/', skillsCtrl.create);
-router.delete('/:id', skillsCtrl.delete);
+router.delete('/:id', skillsCtrl.delete); // DELETE : /1
 router.put('/:id', skillsCtrl.update);
 
 module.exports = router;
